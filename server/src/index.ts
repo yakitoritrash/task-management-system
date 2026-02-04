ff9320169a9e45717fe2423ffaf2ef6e2267c254
Arch me <kishudwivedi30@gmail.com>
2026-02-04 18:41:56 +0530

    post

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import taskRoutes from './routes/taskRoutes';
 
 dotenv.config();
 
